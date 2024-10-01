0af811b7a9aa1daee6b751d102e56eb4ee0e26c7
pijush512 <psagorsarker512@gmail.com>
2024-10-02 01:51:52 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+// Sum of two number.
+// Define variables
+$number1 = 10;
+$number2 = 10;
+// Sum of two variables
+$sum = $number1 + $number2;
+// Display the result
+echo "The total sum is: " .$sum;
+// for line break
+echo " <br>";
+// determine three largest number.
+//Define variables
+$a = 10;
+$b = 20;
+$c = 30;
+// find the largest number
+if($a >$b && $a>$c){
+    echo "The largest number is A: " .$a;
+}
+elseif($b>$a && $b>$c){
+    echo "The largest number is B: " .$b;
+}
+else{
+    echo "The largest number is C: " .$c;
+}
+
+
+
+?>
